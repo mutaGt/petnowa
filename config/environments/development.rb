275da0919e4821ce17945c3219f033078893e1da
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-08-03 06:07:19 +0000

    [Update]READMEのターゲットユーザーと主な利用シーン、使用素材の追記と修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "b0f7094304f14993b50cdd5b8e633ccc.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
