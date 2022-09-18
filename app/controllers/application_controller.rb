ba59ddbd6b8ee9694cbbf30a58b666c6dce8d8dd
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-09-18 11:05:26 +0900

    ページ認証一時停止

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  before_action :authenticate_member!, if: :need_auth_page?
+  # before_action :authenticate_member!, if: :need_auth_page?
     
 def render_404
   render template: 'errors/error_404', status: 404, layout: 'error_404', content_type: 'text/html'
 end
 
-  private
+  # private
 
-  def need_auth_page?
-    unless controller_name == 'homes' && action_name =='top'
-      true
-    end
-  end
+  # def need_auth_page?
+  #   unless controller_name == 'homes' && action_name =='top'
+  #     true
+  #   end
+  # end
 end
