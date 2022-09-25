e9439115110aeeeef79be8d63a2d5abdfd3be723
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-09-25 21:20:16 +0900

    タグの表示順変更

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :member
   has_many :review_tags, dependent: :destroy
-  has_many :tags, through: :review_tags
+  has_many :tags, -> {order(id: :asc)}, through: :review_tags
   has_many :comments, dependent: :destroy
   
   
